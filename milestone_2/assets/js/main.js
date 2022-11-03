66f07b731fa6daff778d36aed48bd9dfa318b240
giuliagrfo <giuliagrifo1@gmail.com>
2022-11-03 15:47:05 +0100

    fix active user

--- milestone_2/assets/js/main.js
+++ milestone_2/assets/js/main.js
@@ -1,6 +1,7 @@
 /*
-Visualizzazione dinamica della lista contatti
- - tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato
 */
 
 const {
